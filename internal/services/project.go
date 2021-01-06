ef8c6c9d74ec657a4891551f9e4a4a49aae20712
Clément Blaise <blaise_c@prodgrp.local>
2021-01-06 19:58:58 +0100

    add inventory filtering

--- internal/services/project.go
+++ internal/services/project.go
@@ -3,6 +3,8 @@ package services
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
 )
 
 type Project struct {
@@ -11,18 +13,58 @@ type Project struct {
 	Playbooks   []*Playbook
 }
 
-func (project *Project) FilterByVarsOr(filters map[string]string) (filtered []*Inventory) {
-	if project == nil {
-		return
-	}
+var filterRegex = regexp.MustCompile("(\\w*)(\\W*)(\\w*)")
 
+func ParseFilter(filter string) (key, op, value string) {
+	result := filterRegex.FindStringSubmatch(filter)
+	return result[1], result[2], result[3]
+}
+
+func ConditionEval(left, right, op string) bool {
+	switch op {
+	case "==":
+		return strings.EqualFold(left, right)
+	case "!=":
+		return !strings.EqualFold(left, right)
+	case "$=":
+		return strings.HasSuffix(left, right)
+	case "~=":
+		return strings.Contains(left, right)
+	case "^=":
+		return strings.HasPrefix(left, right)
+	default:
+		log.Fatalf("Unsuported filter operation %s", op)
+		return false
+	}
+}
+
+func AllTrue(a map[string]bool) bool {
+	for _, value := range a {
+		if !value {
+			return false
+		}
+	}
+	return true
+}
+
+func (project *Project) FilterFromVars(filters []string) (filtered []*Inventory) {
 	for _, inventory := range project.Inventories {
 		if inventory.Data != nil {
-			for key, value := range filters {
-				if inventory.Data.Groups["all"].Vars[key] == value {
-					filtered = append(filtered, inventory)
+
+			type condition = string
+			matchFilter := make(map[condition]bool, len(filters))
+
+			for _, filter := range filters {
+				key, op, value := ParseFilter(filter)
+				if ConditionEval(inventory.Data.Groups["all"].Vars[key], value, op) {
+					matchFilter[filter] = true
 				}
 			}
+
+			if AllTrue(matchFilter) {
+				filtered = append(filtered, inventory)
+			}
+
 		}
 	}
 	return
