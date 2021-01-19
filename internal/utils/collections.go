cc7971ecc1d03516b7f827da3d56a10d0dc1c1fa
Clément Blaise <clementblaise@me.com>
2021-01-19 11:21:14 +0100

    Move map util

--- internal/utils/collections.go
+++ internal/utils/collections.go
@@ -60,3 +60,17 @@ func Map(vs []string, f func(string) string) []string {
 	}
 	return vsm
 }
+
+func MapHasAllTrue(a map[string]bool) bool {
+	if len(a) == 0 {
+		return false
+	}
+
+	for _, value := range a {
+		if !value {
+			return false
+		}
+	}
+
+	return true
+}
