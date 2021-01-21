b76ac19a623e7a8eb87cf902689e46514d2830bd
Emmanuel <fortin.emmanuel@gmail.com>
2021-01-21 16:51:22 +0100

    Fix role read test

--- internal/ansible/role_test.go
+++ internal/ansible/role_test.go
@@ -15,7 +15,7 @@ var simpleRole = Role{
 func TestRole_ReadRoleTasks(t *testing.T) {
 
 	t.Run("with a valid path and simple data should have good tags", func(t *testing.T) {
-		simpleRole.ReadRoleTasks("./../test/projectMultiLevel")
+		simpleRole.ReadRoleTasks("./../../test/projectMultiLevel")
 		assert.NotNil(t, simpleRole.Tasks)
 		assert.NotEmpty(t, simpleRole.Tasks)
 	})
