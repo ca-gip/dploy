290b819b749c4c6aae652297451148d79a0dea65
Clément Blaise <clementblaise@me.com>
2021-01-22 10:55:02 +0100

    Fix task_test.go and add DeepEqual test wrapper

--- internal/ansible/task_test.go
+++ internal/ansible/task_test.go
@@ -2,48 +2,34 @@ package ansible
 
 import (
 	"github.com/ca-gip/dploy/internal/utils"
-	"github.com/go-test/deep"
 	"github.com/stretchr/testify/assert"
 	"gopkg.in/yaml.v2"
 	"testing"
 )
 
-const validTask1 = `
+func TestTask(t *testing.T) {
+
+	t.Run("with a valid task data should be equals", func(t *testing.T) {
+		const validTask = `
 - name: Task1
   template: src="source" dest="destination" owner=root
   tags: tasktag1
 `
+		expect := Task{
+			Name: "Task1",
+			Tags: *utils.NewSetFromSlice("tasktag1"),
+		}
 
-const validTaskWithoutName = `
-- template: src="source" dest="destination" owner=root
-  tags: tasktag1
-`
-
-var expectedValidTask1 = Task{
-	Name: "Task1",
-	Tags: *utils.NewSetFromSlice("tasktag1"),
-}
-
-func TestTask(t *testing.T) {
-
-	t.Run("with a valid task data should be equals", func(t *testing.T) {
-		binData := []byte(validTask1)
+		binData := []byte(validTask)
 		var task []Task
 		err := yaml.Unmarshal(binData, &task)
+
 		assert.Nil(t, err)
 		assert.NotNil(t, task)
 		assert.NotEmpty(t, task)
-
-		//deep.CompareUnexportedFields = false
-		if diff := deep.Equal(task[0], expectedValidTask1); diff != nil {
-			t.Error(diff)
-		}
-		assert.Equal(t, task[0], expectedValidTask1)
-
-		// Not so deep ?
-		if diff := deep.Equal(task[0].Tags, expectedValidTask1.Tags); diff != nil {
-			t.Error(diff)
-		}
+		utils.DeepEqual(t, expect, task[0])
+		assert.Equal(t, task[0], expect)
+		utils.DeepEqual(t, task[0].Tags, expect.Tags)
 
 	})
 
@@ -58,9 +44,7 @@ func TestTask(t *testing.T) {
 			Tags: *utils.NewSetFromSlice("tasktag2"),
 		}
 
-		if diff := deep.Equal(left, right); len(diff) != 0 {
-			t.Error(diff)
-		}
+		utils.DeepNotEqual(t, left, right)
 	})
 
 	t.Run("with two different task name should fail", func(t *testing.T) {
@@ -74,12 +58,20 @@ func TestTask(t *testing.T) {
 			Tags: *utils.NewSetFromSlice("tasktag1"),
 		}
 
-		if diff := deep.Equal(left, right); len(diff) == 0 {
-			t.Error(diff)
-		}
+		utils.DeepNotEqual(t, left, right)
 	})
 
 	t.Run("without name should have tags", func(t *testing.T) {
+
+		const validTaskWithoutName = `
+- template: src="source" dest="destination" owner=root
+  tags: tasktag1
+`
+		expected := Task{
+			Name: utils.EmptyString,
+			Tags: *utils.NewSetFromSlice("tasktag1"),
+		}
+
 		binData := []byte(validTaskWithoutName)
 		var task []Task
 		err := yaml.Unmarshal(binData, &task)
@@ -87,15 +79,7 @@ func TestTask(t *testing.T) {
 		assert.Nil(t, err)
 		assert.NotNil(t, task)
 		assert.NotEmpty(t, task)
-
-		expected := Task{
-			Name: utils.EmptyString,
-			Tags: *utils.NewSetFromSlice("tasktag1"),
-		}
-
-		if diff := deep.Equal(expected, task[0]); len(diff) != 0 {
-			t.Error(diff)
-		}
+		utils.DeepEqual(t, expected, task[0])
 	})
 
 }
--- internal/utils/test.go
+++ internal/utils/test.go
@@ -1,6 +1,10 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 const testPath = "./../../test"
 
@@ -8,3 +12,15 @@ var (
 	ProjectMultiLevelPath  = fmt.Sprint(testPath, "/projectMultiLevel")
 	ProjectSimpleLevelPath = fmt.Sprint(testPath, "/projectSimpleLevel")
 )
+
+func DeepEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if eq := reflect.DeepEqual(expected, actual); !eq {
+		t.Errorf("\nStruct not equals \nExpected\t%+v\nActual\t\t%+v\n", expected, actual)
+	}
+}
+
+func DeepNotEqual(t *testing.T, expected interface{}, actual interface{}) {
+	if eq := reflect.DeepEqual(expected, actual); eq {
+		t.Errorf("\nStruct are both \n\t%+v", expected)
+	}
+}
