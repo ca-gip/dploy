9fc2dae2ee158a5f02b6f6c9b165c27e0b549808
Clément Blaise <clementblaise@me.com>
2021-01-26 18:13:36 +0100

    add test

--- cmd/shared_test.go
+++ cmd/shared_test.go
@@ -25,20 +25,46 @@ func TestFilterCompletion(t *testing.T) {
 			path:       ProjectMultiLevelPath,
 			expect:     []string{"customer==", "customer!=", "customer^=", "customer~=", "customer$="},
 		},
+		"multi-level with 'customer' should return var with operators": {
+			toComplete: "customer",
+			path:       ProjectMultiLevelPath,
+			expect:     []string{"customer==", "customer!=", "customer^=", "customer~=", "customer$="},
+		},
 		"multi-level with 'p' should return all vars": {
 			toComplete: "p",
 			path:       ProjectMultiLevelPath,
 			expect:     []string{"platform==", "platform!=", "platform^=", "platform~=", "platform$="},
 		},
-		"multi-level with 'customer' should return var with operators": {
-			toComplete: "",
+
+		"multi-level with 'customer==' should return with values": {
+			toComplete: "customer==",
 			path:       ProjectMultiLevelPath,
-			expect:     []string{"customer", "env", "os", "platform"},
+			expect:     []string{"customer==customer1", "customer==customer2", "customer==customer3"},
 		},
-		"multi-level with 'customer==' should return var with operators": {
-			toComplete: "",
+		"multi-level with 'customer!=' should return with values": {
+			toComplete: "customer!=",
 			path:       ProjectMultiLevelPath,
-			expect:     []string{"customer", "env", "os", "platform"},
+			expect:     []string{"customer!=customer1", "customer!=customer2", "customer!=customer3"},
+		},
+		"multi-level with 'customer$=' should return with values": {
+			toComplete: "customer$=",
+			path:       ProjectMultiLevelPath,
+			expect:     []string{"customer$=customer1", "customer$=customer2", "customer$=customer3"},
+		},
+		"multi-level with 'customer~=' should return with values": {
+			toComplete: "customer~=",
+			path:       ProjectMultiLevelPath,
+			expect:     []string{"customer~=customer1", "customer~=customer2", "customer~=customer3"},
+		},
+		"multi-level with 'customer^=' should return with values": {
+			toComplete: "customer^=",
+			path:       ProjectMultiLevelPath,
+			expect:     []string{"customer^=customer1", "customer^=customer2", "customer^=customer3"},
+		},
+		"multi-level with 'customer%=' should return with nothing": {
+			toComplete: "customer%=",
+			path:       ProjectMultiLevelPath,
+			expect:     []string{},
 		},
 	}
 
@@ -51,7 +77,6 @@ func TestFilterCompletion(t *testing.T) {
 
 }
 
-//
 //func TestTagsCompletion(t *testing.T) {
 //	testCases := map[string]struct {
 //		toComplete string
@@ -75,6 +100,47 @@ func TestFilterCompletion(t *testing.T) {
 //	for testName, testCase := range testCases {
 //		t.Run(testName, func(t *testing.T) {
 //			actual, _ := tagsCompletion(testCase.toComplete, testCase.path, testCase.playbook)
+//			assert.Equal(t, testCase.expect, actual)
+//		})
+//	}
+//
+//}
+
+//func TestPlaybookCompletion(t *testing.T) {
+//	testCases := map[string]struct {
+//		toComplete string
+//		path       string
+//		expect     []string
+//	}{"multi-level with black should return all vars": {
+//		toComplete: "",
+//		path:       ProjectMultiLevelPath,
+//		expect:     []string{"customer", "env", "os", "platform"},
+//	},
+//		"multi-level with 'c' should should return customer with operators": {
+//			toComplete: "c",
+//			path:       ProjectMultiLevelPath,
+//			expect:     []string{"customer==", "customer!=", "customer^=", "customer~=", "customer$="},
+//		},
+//		"multi-level with 'p' should return all vars": {
+//			toComplete: "p",
+//			path:       ProjectMultiLevelPath,
+//			expect:     []string{"platform==", "platform!=", "platform^=", "platform~=", "platform$="},
+//		},
+//		"multi-level with 'customer' should return var with operators": {
+//			toComplete: "",
+//			path:       ProjectMultiLevelPath,
+//			expect:     []string{"customer", "env", "os", "platform"},
+//		},
+//		"multi-level with 'customer==' should return var with operators": {
+//			toComplete: "",
+//			path:       ProjectMultiLevelPath,
+//			expect:     []string{"customer", "env", "os", "platform"},
+//		},
+//	}
+//
+//	for testName, testCase := range testCases {
+//		t.Run(testName, func(t *testing.T) {
+//			actual, _ := playbookCompletion(testCase.toComplete, testCase.path)
 //			assert.Equal(t, testCase.expect, actual)
 //		})
 //	}
